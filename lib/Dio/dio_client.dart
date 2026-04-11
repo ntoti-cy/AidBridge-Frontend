a877bc81e5e110b8d71d9eeb2931a722850a8667
Cynthia Leyian <cynthialeyian20@gmail.com>
2026-04-11 14:19:18 +0300

    Add the Deployed Version

--- lib/Dio/dio_client.dart
+++ lib/Dio/dio_client.dart
@@ -3,9 +3,7 @@ import 'package:dio/dio.dart';
 class DioClient {
   static final Dio dio = Dio(
     BaseOptions(
-      //baseUrl: "http://10.7.21.196:5000/api/auth",
-       baseUrl: "http://127.0.0.1:5000/api/auth",
-
+      baseUrl: "https://aidbridge-backend-38ei.onrender.com"
       
       headers: {
         "Content-Type": "application/json",
